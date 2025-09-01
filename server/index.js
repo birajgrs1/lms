1e5fd55523e1e72403b6478573940ea9377bebcf
birajgrs1 <birajghorasaine11@gmail.com>
2025-09-01 13:59:26 +0545

    fixing....

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+if (process.env.VERCEL !== '1') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
-});
+export default app; 
